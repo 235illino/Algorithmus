628e4eebe3c58e9f670f615e29f96242a2f13490
illia chvyr <235illino@gmail.com>
2025-02-04 15:50:15 +0100

    Solving LeetCode-Problem 977. Squares of a Sorted Array
    Introduce SortedSquaresClass in Arrays namespace with a method
    SortedSquares that returns a sorted array of squares of input
    integers using a two-pointer technique. Add necessary using
    directives.

---
+++ Arrays/977. SquaresOfASortedArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class SortedSquaresClass
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            int l = 0, r = nums.Length - 1, index = nums.Length - 1;
+            while (l <= r)
+            {
+                var leftElm = nums[l] * nums[l];
+                var rightElm = nums[r] * nums[r];
+                if (leftElm > rightElm)
+                {
+                    result[index] = leftElm;
+                    l++;
+                }
+                else
+                {
+                    result[index] = rightElm;
+                    r--;
+                }
+                index--;
+            }
+            return result;
+        }
+    }
+}
