11f61c46b71cf4b34703da8402c937000d96d5ed
illia chvyr <235illino@gmail.com>
2025-02-04 17:13:54 +0100

    Solving LeetCode Problem 26. Remove Duplicates from Sorted Array
    Introduce RemoveDuplicatesClass in Arrays namespace to remove duplicates from a sorted array using a two-pointer technique. Add necessary using directives. The method returns the new length of the array after duplicates are removed.

---
+++ Arrays/26.RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class RemoveDuplicatesClass
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int l = 0;
+            int r = 0;
+            while (r < nums.Length - 1)
+            {
+                r++;
+                if (nums[l] != nums[r])
+                {
+                    l++;
+                    nums[l] = nums[r];
+                }
+            }
+            return l + 1;
+        }
+    }
+}
