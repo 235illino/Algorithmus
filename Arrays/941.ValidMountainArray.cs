b3515cd915ff8e04837b651fc7e39e42b4421333
illia chvyr <235illino@gmail.com>
2025-02-04 17:29:37 +0100

    Solving LeetCode Problem 941. Valid Mountain Array
    Introduce ValidMountainArrayClass in Arrays namespace with a
    ValidMountainArray method. This method checks if an integer
    array forms a valid mountain array by ascending to a peak
    and then descending, ensuring the peak is neither the first
    nor the last element. Added necessary using directives.

---
+++ Arrays/941.ValidMountainArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class ValidMountainArrayClass
+    {
+        public bool ValidMountainArray(int[] arr)
+        {
+            int l = 0;
+            while (l < arr.Length - 1 && arr[l] < arr[l + 1])
+                l++;
+            if (l == 0 || l == arr.Length - 1)
+                return false;
+            while (l < arr.Length - 1 && arr[l] > arr[l + 1])
+                l++;
+            if (l != arr.Length - 1)
+                return false;
+            return true;
+        }
+    }
+}
