a52e27caa365fece4c49713bd549164bf6e26b99
illia chvyr <235illino@gmail.com>
2025-02-04 17:09:16 +0100

    Solving LeetCode Problem 27. Remove Element
    Introduce RemoveElementClass with RemoveElement method to remove
    specified value from array using two-pointer technique.

---
+++ Arrays/27.RemoveElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class RemoveElementClass
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int read = 0, position = nums.Length;
+            while (read < position)
+            {
+                if (nums[read] == val)
+                {
+                    position--;
+                    (nums[read], nums[position]) = (nums[position], nums[read]);
+                }
+                if (nums[read] != val)
+                    read++;
+            }
+            return position;
+        }
+    }
+}
