9a9de582b0ad07c2f62c66db9e4b576ef8dada66
illia chvyr <235illino@gmail.com>
2025-02-04 18:36:06 +0100

    Solving LeetCode Problem 1051. Height Checker
    https://leetcode.com/problems/height-checker/description/

--- Arrays/1051.HeightChecker.cs
+++ Arrays/1051.HeightChecker.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//https://leetcode.com/problems/height-checker/description/
 namespace Arrays
 {
     public class HeightCheckerClass
