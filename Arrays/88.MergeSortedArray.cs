0a963304a9dc2a09676f3cea82a32eb8e9041967
illia chvyr <235illino@gmail.com>
2025-02-04 17:01:54 +0100

    Solving LeetCode Problem 88. Merge Sorted Array
    Introduce MergeClass in Arrays namespace with Merge method.
    The Merge method merges two sorted arrays, nums1 and nums2,
    into nums1 using a reverse traversal approach. Added necessary
    using directives for common namespaces.

---
+++ Arrays/88.MergeSortedArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class MergeClass
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int insert = nums1.Length - 1;
+            n--;
+            m--;
+            while (insert >= 0 && n >= 0)
+            {
+                if (m < 0 || nums1[m] <= nums2[n])
+                {
+                    nums1[insert] = nums2[n];
+                    n--;
+                }
+                else
+                {
+                    nums1[insert] = nums1[m];
+                    m--;
+                }
+                insert--;
+            }
+        }
+    }
+}
