45ece0653c7327fa8a00e3f9875a41aab82c48b3
illia chvyr <235illino@gmail.com>
2025-02-04 17:22:05 +0100

    Solving LeetCode Problem 1346. Check If N and Its Double Exist
    Introduce CheckIfExistClass in Arrays namespace with CheckIfExist method.
    The method uses a HashSet for efficient lookup and handles special case
    for zero. Added necessary using directives and created Arrays namespace.

---
+++ Arrays/1346.CheckIfNandItsDoubleExist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class CheckIfExistClass
+    {
+        public bool CheckIfExist(int[] arr)
+        {
+            HashSet<int> set = new HashSet<int>(arr);
+            var count = 0;
+            foreach (var num in arr)
+            {
+                if (num == 0)
+                    count++;
+                if (count > 1)
+                    return true;
+                else if (num != 0 && set.Contains(num * 2))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
