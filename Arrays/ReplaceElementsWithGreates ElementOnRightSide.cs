cbab59534a9137992b6831bdfec6526e9dd970f5
illia chvyr <235illino@gmail.com>
2025-02-04 17:45:51 +0100

    Solving LeetCode Problem Replace Elements with Greatest Element on Right Side
    https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3259/
    
    Introduce ReplaceElementsClass with ReplaceElements method to
    replace each element in an array with the greatest element on
    its right side, setting the last element to -1. Add necessary
    using directives for common namespaces.

---
+++ Arrays/ReplaceElementsWithGreates ElementOnRightSide.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3259/
+namespace Arrays
+{
+    class ReplaceElementsClass
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            int i = arr.Length - 1;
+            int max = arr[i];
+            arr[i] = -1;
+            while (i > 0)
+            {
+                var temp = arr[i - 1];
+                arr[i - 1] = max;
+                max = Math.Max(temp, max);
+                i--;
+            }
+            return arr;
+
+        }
+    }
+}
