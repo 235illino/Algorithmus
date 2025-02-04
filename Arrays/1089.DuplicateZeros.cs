853558511a27775a7e3dde2a251e6de11ba5f6ad
illia chvyr <235illino@gmail.com>
2025-02-04 17:35:04 +0100

    Solving LeetCode 1089. Duplicate Zeros
    https://leetcode.com/problems/duplicate-zeros/description/
    
    Introduce DuplicateZerosClass in Arrays namespace to handle zero
    duplication in arrays without expanding their size. The new
    DuplicateZeros method uses a two-pass approach to count and
    shift elements. Retain the original implementation as comments
    for reference. Add necessary using directives.

--- Arrays/1089.DuplicateZeros.cs
+++ Arrays/1089.DuplicateZeros.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//https://leetcode.com/problems/duplicate-zeros/description/
 namespace Arrays
 {
     public class DuplicateZerosClass
