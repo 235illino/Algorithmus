a3abb3ad4dc7e791a1d5518afb169f07380aba4a
illia chvyr <235illino@gmail.com>
2025-02-04 15:29:22 +0100

    add LeetCode-Problem 1295. Find Numbers with Even Number of Digits https://leetcode.com/problems/find-numbers-with-even-number-of-digits/

---
+++ Arrays/1295.FindNumbersWithEvenNumberOfDigits.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+namespace Arrays
+{
+    public class Solution
+    {
+        public int FindNumbers(int[] nums)
+        {
+            var result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var count = 0;
+                while (nums[i] > 0)
+                {
+                    nums[i] = nums[i] / 10;
+                    count++;
+                }
+                if (count % 2 == 0)
+                    result++;
+            }
+            return result;
+        }
+    }
+}
