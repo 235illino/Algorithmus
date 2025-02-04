74a639156b72ef895e168bb95cf8e996b125a4be
illia chvyr <235illino@gmail.com>
2025-02-04 18:01:01 +0100

    Solving LeetCode Problem 283. Move Zeroes
    https://leetcode.com/problems/move-zeroes/description/
    
    Introduce MoveZeroesClass in Arrays namespace with MoveZeroes method.
    The method uses a two-pointer approach to move all zeroes in the
    input array to the end while maintaining the relative order of
    non-zero elements. Necessary namespaces are imported.

---
+++ Arrays/283.MoveZeroes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class MoveZeroesClass
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int i = 0;
+            while (i < nums.Length && nums[i] != 0)
+                i++;
+            int l = i, r = i + 1;
+            while (r < nums.Length)
+            {
+                if (nums[r] != 0)
+                {
+                    nums[l] = nums[r];
+                    nums[r] = 0;
+                    l++;
+                }
+                r++;
+            }
+        }
+    }
+}
