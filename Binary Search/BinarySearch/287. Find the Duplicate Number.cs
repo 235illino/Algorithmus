bc5cec0efe9a3ad4011395893125f8d4625dc222
illia chvyr <235illino@gmail.com>
2025-02-06 22:51:25 +0100

    Solving LeetCode Problem 287. Find the Duplicate Number
    https://leetcode.com/problems/find-the-duplicate-number/

---
+++ Binary Search/BinarySearch/287. Find the Duplicate Number.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/find-the-duplicate-number/
+namespace BinarySearch
+{
+    internal class FindDuplicateClass
+    {
+        public int FindDuplicate(int[] nums)
+        {
+
+            //         int tor = nums[0], rab = nums[0];
+            //         do {
+            //             tor = nums[tor];
+            //             rab = nums[nums[rab]];
+
+            //         } while(tor != rab);
+            //         tor = nums[0];
+            //         while(tor != rab){
+            //             tor = nums[tor];
+            //             rab = nums[rab];
+            //         }
+            //         return tor;
+            int bad = 0, good = nums.Length - 1;
+            while (good - bad > 1)
+            {
+                var mid = bad + (good - bad) / 2;
+                var count = nums.Count(el => el <= mid);
+                if (count > mid)
+                {
+                    good = mid;
+                }
+                else
+                {
+                    bad = mid;
+                }
+            }
+            return good;
+        }
+    }
+}
