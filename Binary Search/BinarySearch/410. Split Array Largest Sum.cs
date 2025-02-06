8390f47983a883c1d9dc3758068915e19b9ae610
illia chvyr <235illino@gmail.com>
2025-02-06 22:55:19 +0100

    Solving LeetCode Problem 410. Split Array Largest Sum
    https://leetcode.com/problems/split-array-largest-sum/

---
+++ Binary Search/BinarySearch/410. Split Array Largest Sum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/split-array-largest-sum/
+namespace BinarySearch
+{
+    internal class SplitArrayClass
+    {
+        public int SplitArray(int[] nums, int k)
+        {
+            int bad = nums.Max() - 1, good = nums.Sum();
+            while (good - bad > 1)
+            {
+                var mid = bad + (good - bad) / 2;
+                if (CalcSubArray(nums, mid) <= k)
+                {
+                    good = mid;
+                }
+                else
+                {
+                    bad = mid;
+                }
+            }
+            return good;
+        }
+
+        public int CalcSubArray(int[] nums, int cand)
+        {
+            int sum = 0, subCalc = 0;
+            foreach (var el in nums)
+            {
+                if (sum + el <= cand)
+                {
+                    sum += el;
+                }
+                else
+                {
+                    sum = el;
+                    subCalc++;
+                }
+            }
+            return subCalc + 1;
+        }
+    }
+}
