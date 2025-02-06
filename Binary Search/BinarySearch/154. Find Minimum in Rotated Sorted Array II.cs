c9aa9c4ca1f2ea015b8235af579d92fad7b6043e
illia chvyr <235illino@gmail.com>
2025-02-06 22:17:41 +0100

    Solving LeetCode Problem 154. Find Minimum in Rotated Sorted Array II
    https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/description/

---
+++ Binary Search/BinarySearch/154. Find Minimum in Rotated Sorted Array II.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+namespace BinarySearch
+{
+    internal class FindMinClass
+    {
+        public int FindMin(int[] nums)
+        {
+            int bad = -1, good = nums.Length - 1;
+            while (good - bad > 1)
+            {
+                var mid = bad + (good - bad) / 2;
+                if (nums[mid] < nums[good])
+                {
+                    good = mid;
+                }
+                else if (nums[mid] > nums[good])
+                {
+                    bad = mid;
+                }
+                else
+                {
+                    good--;
+                }
+            }
+            return nums[good];
+
+        }
+    }
+}
