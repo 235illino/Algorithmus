e95fca982bffaed782d229f364e33cf5bd66138a
illia chvyr <235illino@gmail.com>
2025-02-06 22:21:52 +0100

    Solving LeetCode Problem 349. Intersection of Two Arrays
    https://leetcode.com/problems/intersection-of-two-arrays/

---
+++ Binary Search/BinarySearch/349. Intersection of Two Arrays.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/intersection-of-two-arrays/
+namespace BinarySearch
+{
+    internal class IntersectionClass
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            //         if(nums1.Length > nums2.Length)
+            //             Intersection(nums2, nums1);
+            //         List<int> res = new();
+            //         var set1 = new HashSet<int>(nums1);
+            //         Array.Sort(nums2);
+            //         foreach(var el in set1){
+            //             if(BiSearch(nums2, el)){
+            //                 res.Add(el);
+            //             }
+            //         }
+            //         return res.ToArray();
+
+            //     }
+            //     private bool BiSearch(int[] nums, int target){
+            //         int bad = -1, good = nums.Length;
+            //         while(good - bad > 1){
+            //             var mid = bad + (good - bad) / 2;
+            //             if(nums[mid] >= target){
+            //                 good = mid;
+            //             }
+            //             else{
+            //                 bad = mid;
+            //             }
+            //         }
+
+            //         return good == nums.Length ? false : nums[good] == target;
+
+
+
+
+            HashSet<int> nums = new(nums1);
+            nums.IntersectWith(nums2);
+            return nums.ToArray();
+        }
+    }
+}
