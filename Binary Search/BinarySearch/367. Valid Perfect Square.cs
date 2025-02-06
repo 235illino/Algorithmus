f377e65ebb7a2cba2ed436eac18ecaf9aa308574
illia chvyr <235illino@gmail.com>
2025-02-06 22:02:57 +0100

    Solving LeetCode Problem 744. Find Smallest Letter Greater Than Target
    https://leetcode.com/problems/find-smallest-letter-greater-than-target/

---
+++ Binary Search/BinarySearch/367. Valid Perfect Square.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/valid-perfect-square/description/
+namespace BinarySearch
+{
+    internal class PerfectSquare
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            // double bad = 0, good = num;
+            // while(good - bad > 0.00001){
+            //     double mid = bad + (good - bad) / 2.0;
+            //     if(mid >= num / mid){
+            //         good = mid;
+            //     } else{
+            //         bad = mid;
+            //     }
+            // }
+            // return good % 1 < 0.00001;
+            double bad = 0, good = num;
+            double mid = bad + (good - bad) / 2.0;
+            while (good != mid && bad != mid)
+            {
+                if (mid >= num / mid)
+                {
+                    good = mid;
+                }
+                else
+                {
+                    bad = mid;
+                }
+                mid = bad + (good - bad) / 2.0;
+            }
+            return good % 1 == 0;
+        }
+    }
+}
---
+++ Binary Search/BinarySearch/744. Find Smallest Letter Greater Than Target.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+namespace BinarySearch
+{
+    internal class NextGreatestLetterClass
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            int bad = -1, good = letters.Length;
+            while (good - bad > 1)
+            {
+                var mid = bad + (good - bad) / 2;
+                if (letters[mid] > target)
+                {
+                    good = mid;
+                }
+                else
+                {
+                    bad = mid;
+                }
+            }
+
+            return good == letters.Length ? letters[0] : letters[good];
+        }
+    }
+}
