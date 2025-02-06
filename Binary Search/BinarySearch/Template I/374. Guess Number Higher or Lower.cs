ce5713e4521671100a7074f632c3badf6d60ed68
illia chvyr <235illino@gmail.com>
2025-02-06 13:11:39 +0100

    Solving LeetCode Problem 374. Guess Number Higher or Lower
    https://leetcode.com/problems/guess-number-higher-or-lower/description/

--- Binary Search/BinarySearch/Template I/374. Guess Number Higher or Lower.cs
+++ Binary Search/BinarySearch/Template I/374. Guess Number Higher or Lower.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//https://leetcode.com/problems/guess-number-higher-or-lower/description/
 namespace BinarySearch.Template_I
 {
     public abstract class GuessGame
