75b5b83e2d080f307132867dd077fb51da07ae36
illia chvyr <235illino@gmail.com>
2025-02-06 12:00:04 +0100

    Solving LeetCode Problem 69. Sqrt(x).cs
    https://leetcode.com/problems/sqrtx/description/

---
+++ Binary Search/BinarySearch/704. Binary Search.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/binary-search/
+namespace BinarySearch
+{
+    public class SearchClass
+    {
+        public int Search(int[] nums, int target)
+        {
+            int bad = -1, good = nums.Length;
+            while (good - bad > 1)
+            {
+                var mid = bad + (good - bad) / 2;
+                if (nums[mid] >= target)
+                {
+                    good = mid;
+                }
+                else
+                {
+                    bad = mid;
+                }
+            }
+            if (good < nums.Length && nums[good] == target)
+            {
+                return good;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ Binary Search/BinarySearch/Template I/69. Sqrt(x).cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/sqrtx/description/
+namespace BinarySearch.Template_I
+{
+    public class MySqrtClass
+    {
+        public int MySqrt(int x)
+        {
+            if (x == 0)
+                return 0;
+            if (x == 1)
+                return 1;
+            int bad = 0, good = x;
+            while (good >= bad)
+            {
+                var mid = bad + (good - bad) / 2;
+                if (mid == x / mid)
+                {
+                    return mid;
+                }
+                else if (mid > x / mid)
+                {
+                    good = mid - 1;
+                }
+                else
+                {
+                    bad = mid + 1;
+                }
+            }
+            return good;
+        }
+    }
+}
