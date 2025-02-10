460bf571a499d2e9eb49d6d0eeca8a04ccb27d4e
illia chvyr <235illino@gmail.com>
2025-02-10 11:35:24 +0100

    Solving LeetCode Problem 380. Insert Delete GetRandom O(1)
    https://leetcode.com/problems/insert-delete-getrandom-o1/description/

---
+++ Hash Table/HashTable/380. Insert Delete GetRandom O(1).cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/insert-delete-getrandom-o1/
+namespace HashTable
+{
+    public class RandomizedSet
+    {
+        private Dictionary<int, int> dic;
+        private List<int> list;
+        private int count;
+
+        public RandomizedSet()
+        {
+            dic = new();
+            list = new();
+            count = 0;
+        }
+
+        public bool Insert(int val)
+        {
+            if (dic.ContainsKey(val))
+                return false;
+
+            dic.Add(val, list.Count);
+            list.Add(val);
+
+            return true;
+        }
+
+        public bool Remove(int val)
+        {
+            if (!dic.TryGetValue(val, out int value)) return false;
+
+            if (value != list.Count - 1)
+                list[value] = list[^1];
+
+            dic[list[^1]] = value;
+            list.RemoveAt(list.Count - 1);
+            dic.Remove(val);
+            return true;
+        }
+
+        public int GetRandom()
+        {
+            return list[new Random().Next(list.Count)];
+        }
+    }
+}
---
+++ Hash Table/HashTable/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using HashTable;
+
+RandomizedSet randomizerSet = new RandomizedSet();
+
+
+randomizerSet.Insert(3);
+randomizerSet.Insert(3);
+randomizerSet.GetRandom();
+randomizerSet.GetRandom();
+
+randomizerSet.Insert(1);
+randomizerSet.Remove(3);
+randomizerSet.GetRandom();
+randomizerSet.GetRandom();
+
+randomizerSet.Insert(0);
+randomizerSet.Remove(0);
+
