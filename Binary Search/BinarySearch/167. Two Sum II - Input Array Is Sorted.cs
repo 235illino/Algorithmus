394c352389a5e20482b63c8778c1ecece91b08a1
illia chvyr <235illino@gmail.com>
2025-03-16 21:50:43 +0100

    Solving LeetCode Problems: 350. Intersection of Two Arrays II 167. Two Sum II - Input Array Is Sorted

---
+++ Binary Search/BinarySearch/167. Two Sum II - Input Array Is Sorted.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+namespace BinarySearch
+{
+    class TwoSumClass
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            Dictionary<int, int> seen = new();
+            var count = 1;
+            foreach (var el in numbers)
+            {
+                var cand = target - el;
+                if (seen.ContainsKey(cand))
+                {
+                    return [seen[cand], count];
+                }
+                else
+                {
+                    seen[el] = count;
+                }
+                count++;
+            }
+            return [];
+        }
+    }
+}
---
+++ Binary Search/BinarySearch/350. Intersection of Two Arrays II.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/intersection-of-two-arrays-ii/
+namespace BinarySearch
+{
+    class IntersectClassII
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            if (nums1.Length > nums2.Length)
+                Intersect(nums2, nums1);
+            Dictionary<int, int> dic = new();
+            List<int> res = new();
+            foreach (var num in nums1)
+            {
+                if (!dic.TryAdd(num, 1))
+                {
+                    dic[num]++;
+                }
+            }
+            foreach (var num in nums2)
+            {
+                if (dic.ContainsKey(num))
+                {
+                    res.Add(num);
+                    dic[num]--;
+                    if (dic[num] == 0)
+                        dic.Remove(num);
+                }
+
+                if (dic.Count == 0)
+                    break;
+            }
+            return res.ToArray();
+
+
+            //         if(nums1.Length > nums2.Length)
+            //             return Intersect(nums2, nums1);
+            //         List<int> arr = new();
+            //         Dictionary<int, int> seen = new();
+            //         foreach(var el in nums2){
+            //             if(!seen.TryAdd(el, 1)){
+            //                 seen[el]++;
+            //             }
+            //         }
+            //         foreach(var el in nums1){
+            //             if(seen.ContainsKey(el) && seen[el] > 0){
+            //                 seen[el]--;
+            //                 arr.Add(el);
+            //             }
+            //         }
+            //         return arr.ToArray();
+        }
+    }
+}
